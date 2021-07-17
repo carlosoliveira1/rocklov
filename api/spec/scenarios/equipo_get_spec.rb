af4c465230f2432211f17692bbac0d187fd117f2
Carlos Oliveira <carlosoliveiramauricio@gmail.com>
2021-07-17 15:36:50 -0300

    voltando ao curso

---
+++ api/spec/scenarios/equipo_get_spec.rb
@@ -0,0 +1,103 @@
+describe "GET /equipos{equipo_id}" do
+  before(:all) do
+    payload = { email: "to@mate.com", password: "pwd123" }
+    #criar função deletar esse cadastro, pos se rodar o teste uma segunda vez vai dar erro.
+    result = Sessions.new.login(payload)
+    @user_id = result.parsed_response["_id"]
+  end
+
+  context "Obter unico equipo" do
+    before(:all) do
+      #dado que eu tenho um novo equipamento
+      @payload = {
+        thumbnail: Helpers::get_thumb("sanfona.jpg"),
+        name: "Sanfona",
+        category: "Outros",
+        price: 499,
+
+      }
+      MongoDB.new.remove_equipo(@payload[:name], @user_id)
+      # e eu tenho o id desse equipamento
+
+      equipo = Equipos.new.create(@payload, @user_id)
+      @equipo_id = equipo.parsed_response["_id"]
+
+      #quando faço uma requisição get por id
+      @result = Equipos.new.find_by_id(@equipo_id, @user_id)
+    end
+
+    it "deve retornar 200" do
+      expect(@result.code).to eql 200
+    end
+
+    it "deve retornar o nome" do
+      expect(@result.parsed_response).to include("name" => @payload[:name])
+      #puts @result
+    end
+  end
+
+  context "equipo não existe" do
+    before(:all) do
+      @result = Equipos.new.find_by_id(MongoDB.new.get_mongo_id, @user_id)
+    end
+
+    it "deve retornar 404" do
+      expect(@result.code).to eql 404
+    end
+  end
+end
+
+describe "GET /equipos" do
+  before(:all) do
+    payload = { email: "penelope@gmail.com", password: "pwd123" }
+    #criar função deletar esse cadastro, pos se rodar o teste uma segunda vez vai dar erro.
+    result = Sessions.new.login(payload)
+    @user_id = result.parsed_response["_id"]
+  end
+
+  context "Obter uma lista" do
+    before(:all) do
+      # dado que eu tenho uma lista de equipos
+      payloads = [
+
+        {
+          thumbnail: Helpers::get_thumb("sanfona.jpg"),
+          name: "Sanfona",
+          category: "Outros",
+          price: 499,
+
+        }, {
+          thumbnail: Helpers::get_thumb("trompete.jpg"),
+          name: "Trompete",
+          category: "Outros",
+          price: 599,
+
+        }, {
+          thumbnail: Helpers::get_thumb("slash.jpg"),
+          name: "Les Paul do Slash",
+          category: "Outros",
+          price: 699,
+
+        },
+      ]
+
+      payloads.each do |payload|
+        MongoDB.new.remove_equipo(payload[:name], @user_id)
+        Equipos.new.create(payload, @user_id)
+      end
+
+      # quando eu faço uma requisção get para /equipos
+
+      @result = Equipos.new.list(@user_id)
+    end
+    it "deve retornar 200" do
+      expect(@result.code).to eql 200
+    end
+
+    it "deveretornar uma lista de equipos" do
+      expect(@result.parsed_response).not_to be_empty
+      #puts @result.parsed_response
+      #puts @result.parsed_response.class
+    end
+  end
+end
---
+++ api/spec/scenarios/session_post_spec.rb
@@ -0,0 +1,85 @@
+describe "POST /sessions" do
+  context "login com sucesso" do
+    before(:all) do
+      payload = { email: "betão@hotmail.com", password: "pwd123" }
+      #Não esquecer de cadastrar o Betão na base de dados, caso contrário haverá erro na execução
+
+      @result = Sessions.new.login(payload)
+    end
+
+    it "valida status code" do
+      expect(@result.code).to eql 200
+    end
+
+    it "valida id do usuário" do
+      expect(@result.parsed_response["_id"].length).to eql 24
+    end
+  end
+
+  # examples = [
+
+  #   {
+  #     title: "senha incorreta",
+  #     payload: { email: "betão@yahoo.com", password: "123456" },
+  #     code: 401,
+  #     error: "Unauthorized",
+
+  #   },
+  #   {
+
+  #     title: "usuario não existe",
+  #     payload: { email: "404@yahoo.com", password: "123456" },
+  #     code: 401,
+  #     error: "Unauthorized",
+
+  #   },
+  #   {
+
+  #     title: "email em branco",
+  #     payload: { email: "", password: "123456" },
+  #     code: 412,
+  #     error: "required email",
+
+  #   },
+  #   {
+  #     title: "sem campo email ",
+  #     payload: { password: "123456" },
+  #     code: 412,
+  #     error: "required email",
+
+  #   },
+  #   {
+  #     title: " senha em branco",
+  #     payload: { email: "betão@yahoo.com", password: "" },
+  #     code: 412,
+  #     error: "required password",
+
+  #   },
+  #   {
+  #     title: "sem o campo senha",
+  #     payload: { email: "betão@yahoo.com" },
+  #     code: 412,
+  #     error: "required password",
+
+  #   },
+
+  # ]
+
+  examples = Helpers::get_fixture("login")
+
+  examples.each do |e|
+    context "#{e[:title]}" do
+      before(:all) do
+        @result = Sessions.new.login(e[:payload])
+      end
+
+      it "valida status code #{e[:code]}" do
+        expect(@result.code).to_json eql e[:code]
+      end
+
+      it "valida id do usuário" do
+        expect(@result.parsed_response["error"]).to eql e[:error]
+      end
+    end
+  end
+end
